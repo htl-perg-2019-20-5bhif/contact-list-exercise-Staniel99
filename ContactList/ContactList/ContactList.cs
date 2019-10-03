78771a6a061ed467bfd507f0c09a389b5e9d2fa9
Stefan Daniel <stefan.daniel99@gmx.at>
2019-10-03 08:42:04 +0200

    finished homework

---
+++ ContactList/ContactList/ContactList.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    [ApiController]
+    [Route("contactlist")]
+    public partial class ToDoController : ControllerBase
+    {
+        private static readonly List<contact> contactList =
+            new List<contact> { new contact(1, "herbert", "müller", "mueller@mail.at"), new contact(2, "Franz", "Bauer", "f.bauer@mail.at")};
+
+        [HttpGet]
+        public IActionResult GetAllItems()
+        {
+            return Ok(contactList);
+        }
+
+        [HttpPost]
+        public IActionResult AddItem([FromBody] contact newItem)
+        {
+            contactList.Add(newItem);
+            return CreatedAtRoute("GetSpecificItem", new { index = contactList.IndexOf(newItem) }, newItem);
+        }
+
+        [HttpDelete]
+        [Route("{index}")]
+        public IActionResult DeleteItem(contact item)
+        {
+            if (item.id >= 0 && item.id < contactList.Count)
+            {
+                contactList.RemoveAt(item.id);
+                return NoContent();
+            }
+
+            return BadRequest("Invalid Contact Id");
+        }
+
+        [HttpGet]
+        [Route("{index}", Name = "GetSpecificItem")]
+        public IActionResult GetItem(string lastName)
+        {
+            foreach(contact i in contactList)
+            {
+                if (i.lastName.Equals(lastName))
+                {
+                    return Ok(i);
+                }
+            }
+
+            return BadRequest("Invalid lastName");
+        }
+    }
+}
---
+++ ContactList/ContactList/contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    public class contact
+    {
+        public contact(int id, string firstName, string lastName, string email)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+        }
+
+        [Required]
+        public int id { get; set; }
+
+        [MaxLength(50)]
+        public string firstName { get; set; }
+
+        [MaxLength(50)]
+        public string lastName { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string email { get; set; }
+    }
+}
